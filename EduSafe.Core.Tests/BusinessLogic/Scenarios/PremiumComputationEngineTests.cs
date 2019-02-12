52fcacb3e258e896fe103d1562a749b359789640
Matthew M <3m.moore@gmail.com>
2019-02-12 14:55:41 -0800

    Added a test for the newly corrected bug in enrollment model logic when zero percent rates are targeted

--- EduSafe.Core.Tests/BusinessLogic/Scenarios/PremiumComputationEngineTests.cs
+++ EduSafe.Core.Tests/BusinessLogic/Scenarios/PremiumComputationEngineTests.cs
@@ -4,14 +4,18 @@ using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using EduSafe.Core.Repositories;
 using EduSafe.Core.Tests.BusinessLogic.Models.Premiums;
+using EduSafe.Common.Enums;
 
 namespace EduSafe.Core.Tests.BusinessLogic.Scenarios
 {
     [TestClass]
     public class PremiumComputationEngineTests
     {
-        private const string _dataFile = "EduSafe.Core.Tests.Resources.Dropout-Rate-Test-Scenarios-Single.xlsx";
-        private static Stream _inputFileStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(_dataFile);
+        private const string _dataFileOne = "EduSafe.Core.Tests.Resources.Dropout-Rate-Test-Scenarios-Single.xlsx";
+        private const string _dataFileTwo = "EduSafe.Core.Tests.Resources.EduSafe-Website-For-Profit-Scenario-Testing-Small.xlsx";
+
+        private static Stream _inputFileStreamOne = Assembly.GetExecutingAssembly().GetManifestResourceStream(_dataFileOne);
+        private static Stream _inputFileStreamTwo = Assembly.GetExecutingAssembly().GetManifestResourceStream(_dataFileTwo);
 
         private bool _outputExcel = false;
         private double _precision = 1e-8;
@@ -19,7 +23,7 @@ namespace EduSafe.Core.Tests.BusinessLogic.Scenarios
         [TestMethod, Owner("Matthew Moore")]
         public void PremiumComputationEngine_LoadSingleScenarioFromExcel_CheckResults()
         {
-            var premiumComputationRepository = new PremiumComputationRepository(_inputFileStream);
+            var premiumComputationRepository = new PremiumComputationRepository(_inputFileStreamOne);
             var premiumComputationScenarios = premiumComputationRepository.GetPremiumComputationScenarios();
             var premiumComputationScenario = premiumComputationScenarios.Single();
             var premiumComputationResult = premiumComputationScenario.Value.ComputePremiumResult();
@@ -41,12 +45,39 @@ namespace EduSafe.Core.Tests.BusinessLogic.Scenarios
         [TestMethod, Owner("Matthew Moore")]
         public void PremiumComputationEngine_LoadSingleScenarioByIdFromExcel_CheckResults()
         {
-            var premiumComputationRepository = new PremiumComputationRepository(_inputFileStream);
+            var premiumComputationRepository = new PremiumComputationRepository(_inputFileStreamOne);
             var premiumComputationScenario = premiumComputationRepository.GetPremiumComputationScenarioById(1);
             var premiumComputationResult = premiumComputationScenario.ComputePremiumResult();
 
             var premium = premiumComputationResult.CalculatedMonthlyPremium;
             Assert.AreEqual(85.94250024, premium, _precision);
+
+            if (_outputExcel)
+            {
+                var excelFileWriter = PremiumCalculationTests.CreateExcelOutput(
+                    premiumComputationScenario.RepricingModel.EnrollmentModel.EnrollmentStateTimeSeries,
+                    premiumComputationScenario.PremiumCalculation,
+                    premiumComputationResult.ServicingCosts);
+
+                excelFileWriter.ExportWorkbook();
+            }
+        }
+
+        [TestMethod, Owner("Matthew Moore")]
+        public void PremiumComputationEngine_SingleScenarioWithZeroGradSchoolTarget_CheckResults()
+        {
+            var premiumComputationRepository = new PremiumComputationRepository(_inputFileStreamTwo);
+            var premiumComputationScenario = premiumComputationRepository.GetPremiumComputationScenarioById(2);
+            var premiumComputationResult = premiumComputationScenario.ComputePremiumResult();
+
+            var premium = premiumComputationResult.CalculatedMonthlyPremium;
+            Assert.AreEqual(115.85580399, premium, _precision);
+
+            var gradSchoolRate = premiumComputationScenario
+                .RepricingModel.EnrollmentModel
+                .EnrollmentStateTimeSeries.Last()
+                .GetTotalState(StudentEnrollmentState.GraduateSchool);
+            Assert.AreEqual(0.0, gradSchoolRate, _precision);
 
             if (_outputExcel)
             {
