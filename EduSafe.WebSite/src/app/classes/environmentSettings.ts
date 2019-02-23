5b7799eaec7c3afebaed911855038c5d6e27ea97
Matthew M <3m.moore@gmail.com>
2019-02-23 00:01:34 -0800

    Added keywords to the site and published the WebApi, awesome stuff

--- EduSafe.WebSite/src/app/classes/environmentSettings.ts
+++ EduSafe.WebSite/src/app/classes/environmentSettings.ts
@@ -1,5 +1,6 @@
 export class EnvironmentSettings {
   public static BaseUrl = '//localhost:4200';
-  public static BaseApiUrl = '//localhost:57097';
+  // public static BaseApiUrl = '//localhost:57097';
+  public static BaseApiUrl = 'https://edusafe-webapi.azurewebsites.net';
   public static IpAddressCaptureUrl = 'http://api.ipify.org/';
 }
