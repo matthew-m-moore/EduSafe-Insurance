b6dcd892caf4e4e4d6be915c05406e011f3e9791
Matthew M <3m.moore@gmail.com>
2019-08-16 07:43:32 -0700

    EDSF-72: Tweaking the optics of the institutions home page to get it to look acceptable

---
+++ Database Repository/EdusafeWebAppDB Repository/1. Tables/dbo.WebSiteInquiryInstitutionalInputs.Table.sql
@@ -0,0 +1,17 @@
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'WebSiteInquiryAnswersToQuestions')
+	BEGIN
+		CREATE TABLE WebSiteInquiryAnswersToQuestions
+		(
+			Id int IDENTITY(1,1)
+			, IpAddressId int not null
+			, CollegeNameId int not null
+			, CollegeTypeId int not null
+			, MajorId int not null
+			, CollegeStartDate datetime not null
+			, GraduationDate datetime not null
+			, AnnualCoverage float not null
+			, CreatedOn datetime not null
+			, CreatedBy varchar(25) not null
+			CONSTRAINT PK_AnswersToQuestions PRIMARY KEY (Id)
+		)
+	END
